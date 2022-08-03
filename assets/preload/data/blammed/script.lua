e5d93e80c54f0b8205d4f6e4c7a142baea6201ca
LEVIGHAZALI64 <96037133+LEVIGHAZALI64@users.noreply.github.com>
2022-08-03 19:48:21 +0700

    Add files via upload

--- assets/preload/data/blammed/script.lua
+++ assets/preload/data/blammed/script.lua
@@ -1,71 +1,27 @@
-local xx = 420.95;
-local yy = 513;
-local xx2 = 952.9;
-local yy2 = 550;
-local ofs = 60;
-local followchars = true;
-local del = 0;
-local del2 = 0;
+local keepScroll = false
+function onCreate()
+setProperty('camHUD.visible',false)
+setProperty('notes.cameras',true)
+setProperty('strumLineNotes.cameras',true)
+setProperty('gf.alpha', 0)
+-- setProperty('grpNoteSplashes.cameras',true) idk how to fix this, they bug out
+setProperty('doof.cameras',true)
 
-function onUpdate()
-    if followchars == true then
-        if mustHitSection == false then
-            setProperty('defaultCamZoom',0.8)
-            if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
-                triggerEvent('Camera Follow Pos',xx-ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
-                triggerEvent('Camera Follow Pos',xx+ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singUP' then
-                triggerEvent('Camera Follow Pos',xx,yy-ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
-                triggerEvent('Camera Follow Pos',xx,yy+ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singLEFT-alt' then
-                triggerEvent('Camera Follow Pos',xx-ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singRIGHT-alt' then
-                triggerEvent('Camera Follow Pos',xx+ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singUP-alt' then
-                triggerEvent('Camera Follow Pos',xx,yy-ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singDOWN-alt' then
-                triggerEvent('Camera Follow Pos',xx,yy+ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
-                triggerEvent('Camera Follow Pos',xx,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'idle' then
-                triggerEvent('Camera Follow Pos',xx,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'hey' then
-                triggerEvent('Camera Follow Pos',xx - 130,yy)
-            end
-        else
-            setProperty('defaultCamZoom',1.0)
-            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
-                triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
-                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
-                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
-                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'idle-alt' then
-                triggerEvent('Camera Follow Pos',xx2,yy2)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
-                triggerEvent('Camera Follow Pos',xx2,yy2)
-            end
-        end
-    else
-        triggerEvent('Camera Follow Pos','','')
-    end
+setPropertyFromClass('GameOverSubstate', 'characterName', 'defeatDeath'); --Character json file for the death animation
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'loss-defeat'); --put in mods/sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOverEmpty'); --put in mods/music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEndEmpty'); --put in mods/music/
+	if getPropertyFromClass('ClientPrefs', 'middleScroll') == true then
+		keepScroll = true;
+	elseif getPropertyFromClass('ClientPrefs', 'middleScroll') == false then
+		setPropertyFromClass('ClientPrefs', 'middleScroll', true);
+	end
+end
+
+function onDestroy()
+	if keepScroll == false then
+		setPropertyFromClass('ClientPrefs', 'middleScroll', false);
+	elseif keepScroll == true then
+		keepScroll = false;
+	end
 end
