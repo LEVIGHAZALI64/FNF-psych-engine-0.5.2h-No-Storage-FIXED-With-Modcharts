6e4e06750398ca18d5c618268390fea222d5292b
LEVIGHAZALI64 <96037133+LEVIGHAZALI64@users.noreply.github.com>
2022-08-03 19:47:43 +0700

    Add files via upload

--- assets/preload/data/philly-nice/script.lua
+++ assets/preload/data/philly-nice/script.lua
@@ -1,71 +1,44 @@
-local xx = 420.95;
-local yy = 513;
-local xx2 = 952.9;
-local yy2 = 550;
-local ofs = 60;
-local followchars = true;
-local del = 0;
-local del2 = 0;
+local angleshit = 1;
+local anglevar = 1;
+function onCreate()
+	makeLuaSprite('bg','dmbg',-343.15, -295.45)
+	addLuaSprite('bg',false)
+	
+end
 
-function onUpdate()
-    if followchars == true then
-        if mustHitSection == false then
-            setProperty('defaultCamZoom',0.8)
-            if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
-                triggerEvent('Camera Follow Pos',xx-ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
-                triggerEvent('Camera Follow Pos',xx+ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singUP' then
-                triggerEvent('Camera Follow Pos',xx,yy-ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
-                triggerEvent('Camera Follow Pos',xx,yy+ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singLEFT-alt' then
-                triggerEvent('Camera Follow Pos',xx-ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singRIGHT-alt' then
-                triggerEvent('Camera Follow Pos',xx+ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singUP-alt' then
-                triggerEvent('Camera Follow Pos',xx,yy-ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singDOWN-alt' then
-                triggerEvent('Camera Follow Pos',xx,yy+ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
-                triggerEvent('Camera Follow Pos',xx,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'idle' then
-                triggerEvent('Camera Follow Pos',xx,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'hey' then
-                triggerEvent('Camera Follow Pos',xx - 130,yy)
-            end
-        else
-            setProperty('defaultCamZoom',1.0)
-            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
-                triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
-                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
-                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
-                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'idle-alt' then
-                triggerEvent('Camera Follow Pos',xx2,yy2)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
-                triggerEvent('Camera Follow Pos',xx2,yy2)
-            end
-        end
-    else
-        triggerEvent('Camera Follow Pos','','')
-    end
+
+function onBeatHit()
+	if curBeat < 9999999999999999 then
+		triggerEvent('Add Camera Zoom', 0.04,0.05)
+
+		if curBeat % 2 == 0 then
+			angleshit = anglevar;
+		else
+			angleshit = -anglevar;
+		end
+		setProperty('camHUD.angle',angleshit*3)
+		setProperty('camGame.angle',angleshit*3)
+		doTweenAngle('turn', 'camHUD', angleshit, stepCrochet*0.002, 'circOut')
+		doTweenX('tuin', 'camHUD', -angleshit*8, crochet*0.001, 'linear')
+		doTweenAngle('tt', 'camGame', angleshit, stepCrochet*0.002, 'circOut')
+		doTweenX('ttrn', 'camGame', -angleshit*8, crochet*0.001, 'linear')
+	else
+		setProperty('camHUD.angle',0)
+		setProperty('camHUD.x',0)
+		setProperty('camHUD.x',0)
+	end
+		
+end
+
+function onStepHit()
+	if curBeat < 388 then
+		if curStep % 4 == 0 then
+			doTweenY('rrr', 'camHUD', -12, stepCrochet*0.002, 'circOut')
+			doTweenY('rtr', 'camGame.scroll', 12, stepCrochet*0.002, 'sineIn')
+		end
+		if curStep % 4 == 2 then
+			doTweenY('rir', 'camHUD', 0, stepCrochet*0.002, 'sineIn')
+			doTweenY('ryr', 'camGame.scroll', 0, stepCrochet*0.002, 'sineIn')
+		end
+	end
 end
