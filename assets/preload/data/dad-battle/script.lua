575440b9a33c81b03515e2dd026b1f97fba99d8d
LEVIGHAZALI64 <96037133+LEVIGHAZALI64@users.noreply.github.com>
2022-08-03 19:43:35 +0700

    Add files via upload

--- assets/preload/data/dad-battle/script.lua
+++ assets/preload/data/dad-battle/script.lua
@@ -1,71 +1,23 @@
-local xx = 420.95;
-local yy = 513;
-local xx2 = 952.9;
-local yy2 = 550;
-local ofs = 60;
-local followchars = true;
-local del = 0;
-local del2 = 0;
+local defaultNotePos = {}
+local spin = 7 -- how much it moves before going the other direction
+ 
+function onSongStart()
+    for i = 0, 7 do
+        defaultNotePos[i] = {
+            getPropertyFromGroup('strumLineNotes', i, 'x'),
+            getPropertyFromGroup('strumLineNotes', i, 'y')
+        }
+    end
+end
 
-function onUpdate()
-    if followchars == true then
-        if mustHitSection == false then
-            setProperty('defaultCamZoom',0.8)
-            if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
-                triggerEvent('Camera Follow Pos',xx-ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
-                triggerEvent('Camera Follow Pos',xx+ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singUP' then
-                triggerEvent('Camera Follow Pos',xx,yy-ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
-                triggerEvent('Camera Follow Pos',xx,yy+ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singLEFT-alt' then
-                triggerEvent('Camera Follow Pos',xx-ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singRIGHT-alt' then
-                triggerEvent('Camera Follow Pos',xx+ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singUP-alt' then
-                triggerEvent('Camera Follow Pos',xx,yy-ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singDOWN-alt' then
-                triggerEvent('Camera Follow Pos',xx,yy+ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
-                triggerEvent('Camera Follow Pos',xx,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'idle' then
-                triggerEvent('Camera Follow Pos',xx,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'hey' then
-                triggerEvent('Camera Follow Pos',xx - 130,yy)
-            end
-        else
-            setProperty('defaultCamZoom',1.0)
-            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
-                triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
-                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
-                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
-                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'idle-alt' then
-                triggerEvent('Camera Follow Pos',xx2,yy2)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
-                triggerEvent('Camera Follow Pos',xx2,yy2)
-            end
-        end
-    else
-        triggerEvent('Camera Follow Pos','','')
+function onUpdate(elapsed)
+    local songPos = getPropertyFromClass('Conductor', 'songPosition') / 1000 --How long it will take.
+    
+    if curStep >= 0 and curStep < 999999999999 then
+        setProperty("camHUD.angle", spin * math.sin(songPos))
+    end
+    
+    if curStep == 99999999999999999 then
+        setProperty("camHUD.angle", 0)
     end
 end
