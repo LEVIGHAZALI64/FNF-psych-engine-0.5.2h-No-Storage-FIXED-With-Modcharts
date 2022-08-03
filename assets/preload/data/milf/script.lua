ba8e9c48eadc9af210b73e98cac8b452e48da191
LEVIGHAZALI64 <96037133+LEVIGHAZALI64@users.noreply.github.com>
2022-08-03 19:50:16 +0700

    Add files via upload

--- assets/preload/data/milf/script.lua
+++ assets/preload/data/milf/script.lua
@@ -1,71 +1,36 @@
-local xx = 420.95;
-local yy = 513;
-local xx2 = 952.9;
-local yy2 = 550;
-local ofs = 60;
-local followchars = true;
-local del = 0;
-local del2 = 0;
+local angleshit = 1.5;
+local anglevar = 1.5;
+function opponentNoteHit(id, direction, noteType, isSustainNote)
+	if difficulty == 2 then
+		if curBeat < 1 then
+			if curBeat > 240 then
+				cameraShake('cam', '0.015', '0.1')
+			end
+		end
+	end
+end
 
-function onUpdate()
-    if followchars == true then
-        if mustHitSection == false then
-            setProperty('defaultCamZoom',0.8)
-            if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
-                triggerEvent('Camera Follow Pos',xx-ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
-                triggerEvent('Camera Follow Pos',xx+ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singUP' then
-                triggerEvent('Camera Follow Pos',xx,yy-ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
-                triggerEvent('Camera Follow Pos',xx,yy+ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singLEFT-alt' then
-                triggerEvent('Camera Follow Pos',xx-ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singRIGHT-alt' then
-                triggerEvent('Camera Follow Pos',xx+ofs,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singUP-alt' then
-                triggerEvent('Camera Follow Pos',xx,yy-ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'singDOWN-alt' then
-                triggerEvent('Camera Follow Pos',xx,yy+ofs)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
-                triggerEvent('Camera Follow Pos',xx,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'idle' then
-                triggerEvent('Camera Follow Pos',xx,yy)
-            end
-            if getProperty('dad.animation.curAnim.name') == 'hey' then
-                triggerEvent('Camera Follow Pos',xx - 130,yy)
-            end
+function onBeatHit()
+    if curBeat > 1 then
+        if curBeat % 2 == 0 then
+            angleshit = anglevar;
         else
-            setProperty('defaultCamZoom',1.0)
-            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
-                triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
-                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
-                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
-                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'idle-alt' then
-                triggerEvent('Camera Follow Pos',xx2,yy2)
-            end
-            if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
-                triggerEvent('Camera Follow Pos',xx2,yy2)
-            end
+            angleshit = -anglevar;
         end
-    else
-        triggerEvent('Camera Follow Pos','','')
+        setProperty('camHUD.angle',angleshit*3)
+        setProperty('camGame.angle',angleshit*3)
+        doTweenAngle('turn', 'camHUD', angleshit, stepCrochet*0.002, 'circOut')
+        doTweenX('tuin', 'camHUD', -angleshit*8, crochet*0.001, 'linear')
+        doTweenAngle('tt', 'camGame', angleshit, stepCrochet*0.002, 'circOut')
+        doTweenX('ttrn', 'camGame', -angleshit*8, crochet*0.001, 'linear')
+    end
+end
+
+function opponentNoteHit()
+    if mustHitSection == false then
+        health = getProperty('health')
+        if getProperty('health') > 0.5 then
+            setProperty('health', health- 0.02);
+        end
     end
 end
